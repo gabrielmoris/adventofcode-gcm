77755bde32a76c71dcaaa155b86bad2c37661b9b
gabriel <gabrielcmoris@gmail.com>
2025-12-03 22:07:42 +0100

    solve day 3. Was complicated

--- src/challenges/2025/3/index.ts
+++ src/challenges/2025/3/index.ts
@@ -13,6 +13,7 @@ export const solutionFn = ({ arg }: { arg: string }) => {
     }
 
     const firstIndex = bank.indexOf(firstHighestBattery.toString());
+
     for (let j = firstIndex + 1; j <= bank.length - 1; j++) {
       const numericBattery = Number(bank[j]);
       if (numericBattery > secondHighestBattery) {
@@ -21,6 +22,35 @@ export const solutionFn = ({ arg }: { arg: string }) => {
     }
 
     joltage += Number(firstHighestBattery.toString() + secondHighestBattery.toString());
+  }
+
+  return joltage;
+};
+
+export const solutionFn2 = ({ arg }: { arg: string }) => {
+  let joltage = 0;
+  const arrayOfBanks = arg.split("\n");
+  for (const bank of arrayOfBanks) {
+    let stackOfBatteries: number[] = [];
+    let removalsLeft = bank.length - 12;
+
+    for (let i = 0; i < bank.length; i++) {
+      const currentBattery = Number(bank[i]);
+
+      while (removalsLeft > 0 && stackOfBatteries[stackOfBatteries.length - 1] < currentBattery) {
+        stackOfBatteries.pop();
+        removalsLeft--;
+      }
+
+      stackOfBatteries.push(currentBattery);
+    }
+
+    while (removalsLeft > 0) {
+      stackOfBatteries.pop();
+      removalsLeft--;
+    }
+
+    joltage += Number(stackOfBatteries);
   }
 
   return joltage;
