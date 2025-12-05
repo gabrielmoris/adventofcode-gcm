5c07a96f416afa87426e036dbf4cb956df18a6ca
gabriel <gabrielcmoris@gmail.com>
2025-12-05 17:24:21 +0100

    start day 5

--- src/challenges/2025/5/index.ts
+++ src/challenges/2025/5/index.ts
@@ -3,5 +3,10 @@ export const solutionFn = ({ arg }: { arg: string }) => {
 
   const [ranges, numbers] = arg.split("\n\n");
 
+  const arrayOfRanges = ranges.split("\n").map((range) => {
+    const [first, last] = range.split("-").map(Number);
+    return { first, last };
+  });
+
   return ranges;
 };
