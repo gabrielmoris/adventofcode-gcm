df751095a6f0b79c26f47fc27d9a01474a4841c7
gabriel <gabrielcmoris@gmail.com>
2025-12-06 14:57:46 +0100

    day 6 a

--- src/challenges/2025/6/index.ts
+++ src/challenges/2025/6/index.ts
@@ -1,5 +1,40 @@
 export const solutionFn = ({ arg }: { arg: string }) => {
   let solution = 0;
 
+  const columns = arg.split("\n");
+  const operators = columns
+    .pop()
+    ?.split(" ")
+    .filter((operator) => operator !== "");
+
+  const numbers = columns.map((column) =>
+    column
+      .split(" ")
+      .map(Number)
+      .filter((num) => num !== 0)
+  );
+
+  let colPosition = 0;
+
+  while (operators && colPosition < operators?.length) {
+    let total = 0;
+    const currOperator = operators[colPosition];
+
+    for (let row = 0; row < numbers.length; row++) {
+      if (currOperator === "*") {
+        if (total === 0) {
+          total = 1;
+        }
+        total *= numbers[row][colPosition];
+      } else if (currOperator === "+") {
+        total += numbers[row][colPosition];
+      }
+    }
+
+    solution += total;
+    total = 0;
+    colPosition++;
+  }
+
   return solution;
 };
