ea7d2aa2752d6a5c40dbb6d14f19007835f0c635
gabriel <gabrielcmoris@gmail.com>
2025-12-08 20:11:11 +0100

    day 7

--- src/challenges/2025/7/index.ts
+++ src/challenges/2025/7/index.ts
@@ -20,3 +20,38 @@ export const solutionFn = ({ arg }: { arg: string }) => {
 
   return numberOfSplits;
 };
+
+export const solutionFn2 = ({ arg }: { arg: string }) => {
+  const lines = arg.split("\n").filter((l) => l.trim());
+  const grid = lines.map((line) => line.split(""));
+  let beams = new Array(grid[0].length).fill(0);
+
+  for (let j = 0; j < grid[0].length; j++) {
+    if (grid[0][j] === "S") {
+      beams[j] = 1;
+      break;
+    }
+  }
+
+  for (let i = 0; i < grid.length - 1; i++) {
+    // 1. I will check future path
+    // 2. I will check if there is a fork beam
+    // 3. I will keep the beams that are not stopped
+    let nextBeams = new Array(grid[0].length).fill(0);
+
+    for (let j = 0; j < grid[i].length; j++) {
+      if (beams[j] > 0) {
+        if (grid[i + 1][j] === "^") {
+          // fork beam
+          if (j > 0) nextBeams[j - 1] += beams[j];
+          if (j + 1 < grid[0].length) nextBeams[j + 1] += beams[j];
+        } else {
+          nextBeams[j] += beams[j];
+        }
+      }
+    }
+    beams = nextBeams;
+  }
+
+  return beams.reduce((sum, b) => sum + b, 0);
+};
