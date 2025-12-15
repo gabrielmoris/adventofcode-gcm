3e712b17f41c755e81ebc58e8d2fb9e37a1e676b
gabriel <gabrielcmoris@gmail.com>
2025-12-15 09:16:00 +0100

    think about current implementation

--- src/challenges/2025/9/index.ts
+++ src/challenges/2025/9/index.ts
@@ -23,3 +23,15 @@ const calculateArea = (vertexPoint1: number[], vertexPoint: number[]) => {
   const height = Math.abs(vertexPoint1[1] - vertexPoint[1]) + 1;
   return width * height;
 };
+
+export const solutionFn2 = ({ arg }: { arg: string }) => {
+  const tiles = arg.split("\n").map((line) => line.split(",").map(Number));
+
+  let area = 0;
+
+  // 1. vheck area
+  // 2. check if the area fullfills the conditions to be included inside green and red tiles with a function
+  // 3. check if it is bigger than current area
+
+  return area;
+};
