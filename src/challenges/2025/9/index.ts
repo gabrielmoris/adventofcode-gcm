b22c1932cac37f2775093037d3bbaa4868a8b00f
gabriel <gabrielcmoris@gmail.com>
2025-12-17 13:59:00 +0100

    think new approach

--- src/challenges/2025/9/index.ts
+++ src/challenges/2025/9/index.ts
@@ -1,9 +1,11 @@
+type Point = [number, number];
+
 export const solutionFn = ({ arg }: { arg: string }) => {
-  const tiles = arg.split("\n").map((line) => line.split(",").map(Number));
+  const tiles: Point[] = arg.split("\n").map((line) => line.split(",").map(Number)) as Point[];
 
   let area = 0;
 
-  tiles.forEach((tile) => {
+  tiles.forEach((tile: Point) => {
     tiles.forEach((secondTile) => {
       if (tile[0] === secondTile[0] && tile[1] === secondTile[1]) {
         return;
@@ -25,7 +27,7 @@ const calculateArea = (vertexPoint1: number[], vertexPoint: number[]) => {
 };
 
 export const solutionFn2 = ({ arg }: { arg: string }) => {
-  const tiles = arg.split("\n").map((line) => line.split(",").map(Number));
+  const tiles = arg.split("\n").map((line) => line.split(",").map(Number)) as Point[];
 
   let area = 0;
 
@@ -35,25 +37,21 @@ export const solutionFn2 = ({ arg }: { arg: string }) => {
         return;
       }
 
-      if (!checkIfAreaIsValid(tile[0], tile[1], tiles) || !checkIfAreaIsValid(secondTile[0], secondTile[1], tiles)) return;
+      if (!checkIfAreaIsValid(tile, secondTile, tiles)) return;
       const currentArea = calculateArea(tile, secondTile);
       if (currentArea > area) {
         area = currentArea;
       }
     });
   });
-  return area; // 4537841076 too high
+  return area; // 4537841076 too high 4750297200
 };
 
-const checkIfAreaIsValid = (x: number, y: number, tiles: number[][]) => {
-  let isValid = false;
+const checkIfAreaIsValid = (corner1: Point, cortner2: Point, tiles: Point[]): boolean => {
+  const xmin = Math.min(corner1[0], cortner2[0]);
+  const xmax = Math.max(corner1[0], cortner2[0]);
+  const ymin = Math.min(corner1[1], cortner2[1]);
+  const ymax = Math.max(corner1[1], cortner2[1]);
 
-  for (let i = 0, j = tiles.length - 1; i < tiles.length; j = i++) {
-    const [xi, yi] = tiles[i];
-    const [xj, yj] = tiles[j];
-
-    //check here If it is omn green or red tile
-  }
-
-  return isValid;
+  return false;
 };
