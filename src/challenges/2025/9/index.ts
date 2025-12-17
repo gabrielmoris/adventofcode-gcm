d045ca390976f26f2b77987dfa23cb262ef4dfb2
gabriel <gabrielcmoris@gmail.com>
2025-12-17 15:54:25 +0100

    add new step to create polygon

--- src/challenges/2025/9/index.ts
+++ src/challenges/2025/9/index.ts
@@ -28,30 +28,63 @@ const calculateArea = (vertexPoint1: number[], vertexPoint: number[]) => {
 
 export const solutionFn2 = ({ arg }: { arg: string }) => {
   const tiles = arg.split("\n").map((line) => line.split(",").map(Number)) as Point[];
+  const coloredSet = createPoligon(tiles);
 
   let area = 0;
 
-  tiles.forEach((tile) => {
-    tiles.forEach((secondTile) => {
-      if (tile[0] === secondTile[0] && tile[1] === secondTile[1]) {
-        return;
-      }
+  // tiles.forEach((tile) => {
+  //   tiles.forEach((secondTile) => {
+  //     if (tile[0] === secondTile[0] && tile[1] === secondTile[1]) return;
 
-      if (!checkIfAreaIsValid(tile, secondTile, tiles)) return;
-      const currentArea = calculateArea(tile, secondTile);
-      if (currentArea > area) {
-        area = currentArea;
+  //     if (!checkIfAreaIsValid(tile, secondTile, coloredSet)) return;
+  //     const currentArea = calculateArea(tile, secondTile);
+  //     if (currentArea > area) area = currentArea;
+  //   });
+  // });
+
+  console.log([...coloredSet]);
+  console.log(coloredSet.has("7,1"));
+  return area;
+};
+
+// const checkIfAreaIsValid = (corner1: Point, cortner2: Point, coloredSet: Set<string>): boolean => {
+//   const xmin = Math.min(corner1[0], cortner2[0]);
+//   const xmax = Math.max(corner1[0], cortner2[0]);
+//   const ymin = Math.min(corner1[1], cortner2[1]);
+//   const ymax = Math.max(corner1[1], cortner2[1]);
+
+//   for (let x = xmin; x <= xmax; x++) {
+//     for (let y = ymin; y <= ymax; y++) {
+//       if (!coloredSet.has(`${x},${y}`)) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
+
+const createPoligon = (tiles: Point[]) => {
+  const polygon = new Set<string>();
+
+  tiles.forEach(([x, y]) => {
+    polygon.add(`${x},${y}`);
+    tiles.forEach(([x2, y2]) => {
+      if (x === x2 && y === y2) return;
+
+      polygon.add(`${x2},${y2}`);
+
+      const minX = Math.min(x, x2);
+      const maxX = Math.max(x, x2);
+      const minY = Math.min(y, y2);
+      const maxY = Math.max(y, y2);
+
+      for (let x = minX; x <= maxX; x++) {
+        for (let y = minY; y <= maxY; y++) {
+          polygon.add(`${x},${y}`);
+        }
       }
     });
   });
-  return area; // 4537841076 too high 4750297200
-};
 
-const checkIfAreaIsValid = (corner1: Point, cortner2: Point, tiles: Point[]): boolean => {
-  const xmin = Math.min(corner1[0], cortner2[0]);
-  const xmax = Math.max(corner1[0], cortner2[0]);
-  const ymin = Math.min(corner1[1], cortner2[1]);
-  const ymax = Math.max(corner1[1], cortner2[1]);
-
-  return false;
+  return polygon;
 };
